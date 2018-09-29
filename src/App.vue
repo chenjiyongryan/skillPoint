5315192165526a6b47b8238759fa54d6e08d7ae5
“chenjiyong” <chenjiyong@zto.cn>
2018-09-29 16:31:42 +0800

    ---

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+  watch: {
+      '$route' (to, from) {
+          console.log('touterChange>>>>',to,from)
+     }
+   },
 }
 </script>
 
---
+++ src/components/mokuaihua.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="module">
+        
+  </div>
+</template>
+
+<script>
+import {a} from './moduleByexport'
+export default {
+  components: {},
+  name: "module",
+  data() {
+    return {
+      show: true
+    };
+  },
+
+  computed: {},
+  mounted() {
+      console.log(a)
+  },
+  
+  methods: {
+    compTest() {}
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
